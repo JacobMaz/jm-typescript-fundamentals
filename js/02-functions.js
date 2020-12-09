99ad8920af20afa43725ed67badce8c66355c966
JakeMaz <jakemazanowski@gmail.com>
2020-12-09 15:16:09 -0500

    all done

--- js/02-functions.js
+++ js/02-functions.js
@@ -2,34 +2,26 @@
 // function demoFunction(someNum){
 //     return someNum;
 // }
-
 // demoFunction(1);
 // demoFunction('stuff');
 // demoFunction(1, 'Stuff');
-
 // function addNumbers(numOne: number, numTwo: number){
 //     return numOne + numTwo;
 // }
 // // Works
 // addNumbers(1,2);
-
 // // Errors
 // addNumbers(1,2,3);
 // addNumbers(1,2,3, 'Foo');
-
 // function fullName(firstName: string, LastName: string){
 //     return firstName + ' ' + LastName;
 // }
-
 // console.log(fullName('Ricky', 'Bobby'));
-
 // function sayHello(name: string) : string {
 //     return name;
 // }
-
 // sayHello('Kenn'); // works
 // sayHello(1); // error
-
 // function practiceFunction(username: string, password: string) : boolean {
 //     if (username === 'elevenfiftyuser' && password === 'Letmein1234!'){
 //         return true
@@ -37,25 +29,22 @@
 //         return false
 //     }
 // }
-
 // console.log(practiceFunction('elevenfiftyuser', 'Letmein1234!'));
 // console.log(practiceFunction('test', 'testing'));
-
 // function addNumbersWithOptional (numOne: number, numTwo: number, someString?: string){
 //     console.log(someString);
 //     return numOne + numTwo;
 // }
-
 // addNumbersWithOptional(1,2);
 // addNumbersWithOptional(1,2,'This is optional');
-
-function fullNameTwo(first: string, last: string, middle?: string){
+function fullNameTwo(first, last, middle) {
     if (middle !== undefined) {
-        return `${first} ${middle} ${last}`
-    } else {
-        return `${first} ${last}`
+        return first + " " + middle + " " + last;
+    }
+    else {
+        return first + " " + last;
     }
 }
-
 console.log(fullNameTwo('test', 'name'));
-console.log(fullNameTwo('test', 'testing', 'tester'))
+console.log(fullNameTwo('test', 'testing', 'tester'));
+//# sourceMappingURL=02-functions.js.map
--- js/03-classes.js
+++ js/03-classes.js
@@ -1,34 +1,23 @@
 // class Person {
 //     firstName: string;
 //     lastName: string;
-
 //     sayHello(){
 //         console.log('Hello', this.firstName);
 //     }
-
 //     sayHelloToFullname(){
 //         console.log('Hello', this.firstName, this.lastName);
 //     }
 // }
-
 // let phil: Person = new Person();
-
 // phil.firstName = 'Phil';
 // phil.lastName = 'Donahue';
-
 // let kenn: Person = new Person();
-
 // kenn.firstName = 'Kenn';
-
 // // kenn.sayHello();
-
 // let sheriff: Person = new Person();
-
 // sheriff.firstName = 'Sheriff';
 // sheriff.lastName = 'Holler';
-
 // sheriff.sayHelloToFullname();
-
 // class Game {
 //     constructor(name: string, maker: string){
 //         this.gameName = name;
@@ -38,7 +27,6 @@
 //     gameMaker: string;
 // }
 // let battleship: Game = new Game('Battleship', 'Hasbro');
-
 // class Vehicle {
 //     type: string;
 //     make: string;
@@ -47,35 +35,28 @@
 //     runs: boolean;
 //     topSpeed: number;
 // }
-
 // class Automobile extends Vehicle {
 //     isSelfDriving: boolean;
 // }
-
 // let myCar: Automobile = new Automobile();
 // myCar.isSelfDriving = false;
 // myCar.topSpeed = 200;
 // myCar.make = 'Volvo';
-
 // class Motorcycle extends Vehicle {
 //     easyToDoWheelie: boolean
 // }
-
 // let myMotorcycle: Motorcycle = new Motorcycle();
 // myMotorcycle.easyToDoWheelie = true;
 // myMotorcycle.topSpeed = 180;
-
 // class Animal {
 //     type: string;
 //     appendages: number;
 //     fur: boolean;
 // }
-
 // class Lion extends Animal {
 //     gender: string;
 //     weight: number;
 // }
-
 // let myLion: Lion = new Lion()
 // myLion.type = 'Lion';
 // myLion.appendages = 4;
@@ -83,27 +64,22 @@
 // myLion.gender = 'male';
 // myLion.weight = 280
 // // console.log(myLion)
-
 // class Deer extends Animal {
 //     antlers: boolean;
 //     location: string;
 // }
-
 // let myDeer: Deer = new Deer()
 // myDeer.type = 'Deer';
 // myDeer.appendages = 4;
 // myDeer.fur = true;
 // myDeer.antlers = true;
 // myDeer.location = 'indiana'
-
 // class Store {
 //     constructor(public name: string, public city: string){}
 // }
 // let ikea: Store = new Store('Ikea', 'Fishers');
-
 // class Employee extends Person{
 //     private _salary: number;
-    
 //     getSalary(): string{
 //         return this._salary.toString();
 //     }
@@ -111,10 +87,8 @@
 //         this._salary = newSalary;
 //     }
 // }
-
 // let newEmployee: Employee = new Employee();
 // newEmployee.setSalary(30000);
-
 // let salaryResult: string = newEmployee.getSalary();
 // console.log('The salary is:', salaryResult);
-
+//# sourceMappingURL=03-classes.js.map
